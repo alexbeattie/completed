9167c0facd3f4b55cd3ee1609424d6046fbf010d
Alex Beattie <artrisanbranding@me.com>
2020-02-13 10:27:52 -0800

    implement service singleton

--- sherwoodrealestate/Service.swift
+++ sherwoodrealestate/Service.swift
@@ -7,53 +7,12 @@
 ////
 //
 import Foundation
-//
 
-
-//struct Service {
-//    struct responseData: Codable {
-//        var D: ResultsData
-//    }
-//    struct ResultsData: Codable {
-//        var Results: [resultsArr]
-//    }
-//    struct resultsArr: Codable {
-//        var AuthToken: String
-//        var Expires: String
-//        
-//    }
-//    static let shared = Service() // singleton
-//    func fetchAuthToken(completion: @escaping (responseData) -> ()) {
-//        var request = URLRequest(url: URL(string: "\(SESSION_URL)")!)
-//        let config = URLSessionConfiguration.default
-//        let session = URLSession(configuration: config)
-//
-//        request.httpMethod = "POST"
-//        request.addValue("SparkiOS", forHTTPHeaderField: "X-SparkApi-User-Agent")
-//
-//        
-//        let task = session.dataTask(with: request as URLRequest) { data, response, error in
-//            if let error = error {
-//                print("Error took place \(error)")
-//                return
-//            }
-// 
-//            if let json = data {
-//                do {
-//                    let tokenResponse = try JSONDecoder().decode(responseData.self, from: json)
-////                    print(tokenResponse.D?.Results)
-//                    _ = tokenResponse.D.Results[0].AuthToken
-////                    print("This is the authToken: \(authToken)")
-//                    
-//                    completion(tokenResponse)
-//                    
-//                    
-//                } catch {
-//                    print(error)
-//                }
-//            }
-//        }
-//            
-//                    task.resume()
-//    }
-//}
+class Service {
+    
+    static let shared = Service()
+    
+    func fetchSherwood() {
+        print("fetched sherwood")
+    }
+}
